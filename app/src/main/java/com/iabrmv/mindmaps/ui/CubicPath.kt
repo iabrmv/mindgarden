9c57086db61e9774fb4af60c702c372cdde8fd44
iabrmv <vancir@gmail.com>
2022-01-13 20:55:40 +0300

    Smooth edges animation supported

--- app/src/main/java/com/iabrmv/mindmaps/ui/CubicPath.kt
+++ app/src/main/java/com/iabrmv/mindmaps/ui/CubicPath.kt
@@ -1,7 +1,17 @@
 package com.iabrmv.mindmaps.ui
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.keyframes
+import androidx.compose.foundation.Canvas
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kotlin.math.abs
 
 fun cubicPath(start: Offset, end: Offset) = Path().apply {
@@ -20,4 +30,45 @@ fun cubicPath(start: Offset, end: Offset) = Path().apply {
             x3 = end.x, y3 = end.y
         )
     }
+}
+
+@Composable
+fun Edge(modifier: Modifier = Modifier, start: Offset, end: Offset, color: Color) {
+    val isConvex = abs(end.y - start.y) < abs(end.x - start.x)
+    val x2 by animateFloatAsState(
+        targetValue = if(isConvex) end.x else start.x,
+        animationSpec = keyframes{ durationMillis = 300 }
+    )
+    val y2 by animateFloatAsState(
+        targetValue = if(isConvex) start.y else end.y,
+        animationSpec = keyframes{ durationMillis = 300 }
+    )
+
+    Canvas(modifier) {
+        val path = Path().apply {
+            moveTo(start.x, start.y)
+            cubicTo(
+                x1 = start.x, y1 = end.y,
+                x2 = x2, y2 = y2,
+                x3 = end.x, y3 = end.y
+            )
+        }
+        drawPath(
+            color = color,
+            path = path,
+            style = Stroke(1.dp.toPx())
+        )
+
+    }
+}
+
+@Preview
+@Composable
+fun CubicPathPreview() {
+//    Box(
+//        Modifier
+//            .fillMaxSize()
+//            .background(Color.White)) {
+//        Edge(Modifier.offset(100.dp, 100.dp).size(100.dp, 50.dp), color = Color.Magenta)
+//    }
 }
